b28f70bfeb07424121b4b968f5fc22694f72c34a
Stanley Chidolue <chidoluechibueze@gmail.com>
2023-11-25 08:01:56 +0100

    modified main.py to handle place_the_bet exceptions

--- main.py
+++ main.py
@@ -57,8 +57,10 @@ while True:
             game_play.choose_market()
             time.sleep(1)
             week_selected=game_play.select_stake_options(week="current_week",previous_week_selected="Week 50")
-
-            acc_bal=game_play.place_the_bet(amount=str(AMOUNT_LIST[n]*GAME_LEVEL),test=True)
+            try:
+                acc_bal=game_play.place_the_bet(amount=str(AMOUNT_LIST[n]*GAME_LEVEL),test=True)
+            except:
+                pass
             # week_selected=game_play.select_stake_options(week="after_current_week",
             #                                             previous_week_selected=week_selected)
             reduced_week_selected=reduce_week_selected(week_selected,by=0,league=LEAGUE["name"])
