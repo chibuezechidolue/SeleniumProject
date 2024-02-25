a4b1bcaf7ce4e22efc5e7ff0534cd8d67d1c0ea3
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-02-25 18:12:04 +0100

    changed the email title

--- brain.py
+++ brain.py
@@ -521,7 +521,7 @@ class CheckPattern:
         if result["outcome"] == True and length.lower() == "all result":
             send_email(Email=os.environ.get("EMAIL_USERNAME"),
                        Password=os.environ.get("EMAIL_PASSWORD"),
-                       Subject=f"{self.market} came in the last SEASON",
+                       Subject=f"11th - 20th Week RESULTS",
                        Message=result["message"],
                     #    File_path=[page_path1]
                        )
