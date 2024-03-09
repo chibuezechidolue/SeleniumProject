ea0becd81628f480e23d8feec66a28a9e45ed8bd
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-03-09 09:17:40 +0100

    activated --headless option for chrome

--- tools.py
+++ tools.py
@@ -14,7 +14,7 @@ def set_up_driver_instance():
     """ To create and return a webdriver object with disabled gpu and headless"""
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_argument("--disable-gpu")
     return webdriver.Chrome(options=chrome_options)
