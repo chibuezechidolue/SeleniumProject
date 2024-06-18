c120f6ab6c9594a0f957d3cb9a5420b3c225a66a
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-06-18 11:57:53 +0100

    fixed a bug in main.py

--- brain.py
+++ brain.py
@@ -42,7 +42,7 @@ class PlayGame:
                 close_more_markets_button.click()
             except:
                 more_markets_button=self.browser.find_element(By.CSS_SELECTOR,'[data-testid="market-dropdown-more-markets"]')
-                self.browser.execute_script(f"window.scrollTo(0, {more_markets_button.location['y']-100});")
+                self.browser.execute_script(f"window.scrollTo(0, {more_markets_button.location['y']-200});")
                 time.sleep(0.5)
                 if check_if_current_week_islive(self.browser):
                     time.sleep(40)
@@ -64,7 +64,7 @@ class PlayGame:
             if check_if_current_week_islive(self.browser):
                 time.sleep(40)
             market_to_select=self.browser.find_element(By.CSS_SELECTOR, market_selector)
-            self.browser.execute_script(f"window.scrollTo(0, {market_to_select.location['y']-100});")
+            self.browser.execute_script(f"window.scrollTo(0, {market_to_select.location['y']-200});")
             time.sleep(0.5)
             # market_to_select = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, market_selector)))
             market_to_select.click()
@@ -78,7 +78,7 @@ class PlayGame:
             try:
                 self.browser.execute_script(f"window.scrollTo(0, 0);")
                 time.sleep(1)
-                # self.browser.execute_script(f"window.scrollTo(0, {close_more_markets_button.location['y']-100});")
+                # self.browser.execute_script(f"window.scrollTo(0, {close_more_markets_button.location['y']-200});")
                 close_more_markets_button.click()
             except:
                 pass
@@ -120,7 +120,7 @@ class PlayGame:
                 except (ElementClickInterceptedException, StaleElementReferenceException, TimeoutException):
                     print("exception was thrown at available_games_1")
                     self.browser.execute_script(
-                        f"window.scrollTo(0, {available_games_1.location['y']-100});")  # To Scroll to where the element can be clicked()
+                        f"window.scrollTo(0, {available_games_1.location['y']-200});")  # To Scroll to where the element can be clicked()
 
                     time.sleep(0.5)
                     available_games_1=available_games[:end][n]
@@ -157,7 +157,7 @@ class PlayGame:
                     except (ElementClickInterceptedException, TimeoutException):
                         print("exception was thrown at stake_option_1")
                         self.browser.execute_script(
-                            f"window.scrollTo(0, {option_btn.location['y']-100});")  # To Scroll to where the element can be clicked()
+                            f"window.scrollTo(0, {option_btn.location['y']-200});")  # To Scroll to where the element can be clicked()
                         time.sleep(0.5)
                         option_btn.click()
                         # time.sleep(0.5)
@@ -176,7 +176,7 @@ class PlayGame:
                         available_games_1=available_games[:end][n]
                         try:
                             self.browser.execute_script(
-                                f"window.scrollTo(0, {available_games_1.location['y']-100});")  # To Scroll to where the element can be clicked()
+                                f"window.scrollTo(0, {available_games_1.location['y']-200});")  # To Scroll to where the element can be clicked()
                             time.sleep(0.5)
                             available_games_1.click()
                             # time.sleep(0.5)
@@ -204,10 +204,10 @@ class PlayGame:
                 clear_bet_slip(self.browser)
                 send_email(Email=os.environ.get("EMAIL_USERNAME"),
                         Password=os.environ.get("EMAIL_PASSWORD"),
-                        Subject="(1st-10th) ERROR during select_stake_options",
+                        Subject="(FullSeason) ERROR during select_stake_options",
                         Message=f"An error occured during select_stake_options. This is the error: {error}"
                         )
-                return week_to_select_text
+                return [week_to_select_text,]
 
     def place_the_bet(self, amount: int, test: bool)->str:
         """ To bet the selected stake options each with the inputed amount"""
@@ -402,6 +402,7 @@ class CheckPattern:
                     page_path2 = "SeleniumProject/saved_pages/eleven_to_twenty_page.html"
                     save_page(self.browser, page_name=page_path2)
                 result = confirm_outcome(ht_scores=ht_scores, ft_scores=ft_scores, game_weeks=game_weeks,market=market,length=length)
+            
             except Exception as error:
                 print("an error occured i skipped this session")
                 print(f"this is the error: {error}")
@@ -432,7 +433,7 @@ class CheckPattern:
                 game_weeks = self.browser.find_elements(By.CSS_SELECTOR, ".week-number")
                 # checking if the last week played is latest_week before going ahead to save the page
                 game_weeks = check_if_last_result_equal_input(self.browser, game_weeks=game_weeks,
-                                                            week_to_check=latest_week,time_delay=10)
+                                                            week_to_check=latest_week,time_delay=5)
                 game_weeks = game_weeks[:1]
 
                 # Re-fill the ht and ft_scores list by the reloaded/current score result of the last week played   
@@ -486,12 +487,14 @@ class CheckPattern:
             return {"outcome": True, "driver": self.browser}
 
         elif result["outcome"] != "" and length.lower() == "all result":
-            send_email(Email=os.environ.get("EMAIL_USERNAME"),
-                       Password=os.environ.get("EMAIL_PASSWORD"),
-                       Subject=f"PATTERN found",
-                       Message=result["message"],
-                       File_path=[page_path1, page_path2]
-                       )
+            print('PATTERN found')
+            print('Did not send mail')
+            # send_email(Email=os.environ.get("EMAIL_USERNAME"),
+            #            Password=os.environ.get("EMAIL_PASSWORD"),
+            #            Subject=f"PATTERN found",
+            #            Message=result["message"],
+            #            File_path=[page_path1, page_path2]
+            #            )
             cancel_result_page_button = self.browser.find_element(By.CSS_SELECTOR, "svg path")
             cancel_result_page_button.click()
             return {"outcome": result["outcome"], "driver": self.browser}
--- main.py
+++ main.py
@@ -104,13 +104,17 @@ while True:
             try:
                 result=game_play.select_stake_options(week="current_week",previous_week_selected="Week 50",pattern_stake=pattern_stake_options[check_result['outcome']],stake_amount=AMOUNT_LIST[n]*GAME_LEVEL)
                 week_selected=result[0]
-                acc_bal=result[1]
+                try:
+                    acc_bal=result[1]
+                except:
+                    pass
             except:
                 pass
             reduced_week_selected=reduce_week_selected(week_selected,by=0,league=LEAGUE["name"])
             
             pattern=CheckPattern(browser,market=SELECTED_MARKET)
-            if pattern.check_result(length="last result",latest_week=reduced_week_selected,acc_balance=acc_bal,market=check_result['outcome']):
+            last_result=pattern.check_result(length="last result",latest_week=reduced_week_selected,acc_balance=acc_bal,market=check_result['outcome'])
+            if last_result['outcome']:
                 # Calculate the number of weeks left before week 10 of the next season
                 won=True
                 weeks_left_to_finish_season = LEAGUE["num_of_weeks"] - int(reduced_week_selected.split()[1])
