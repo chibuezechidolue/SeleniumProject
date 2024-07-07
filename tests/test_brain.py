eb9b3e8b509faca54dd3264a94412adc35a19ff2
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-07-07 14:40:04 +0100

    modified test_select_stake_options for for live server testing

--- tests/test_brain.py
+++ tests/test_brain.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from brain import (PlayGame,CheckPattern,LoginUser)
 from tools import reduce_week_selected,clear_bet_slip, set_up_driver_instance,delete_cache
 from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
 
 load_dotenv()
 
@@ -14,10 +15,10 @@ load_dotenv()
 class BrainTest(unittest.TestCase):
 
     def setUp(self):
-        # test_market="correct_score"
-        test_market="ht/ft"
-        self.browser=webdriver.Chrome()    # driver instance with User Interface (not headless)
-        # self.browser=set_up_driver_instance() # driver instance without User Interface (--headless)
+        test_market="correct_score"
+        # test_market="ht/ft"
+        # self.browser=webdriver.Chrome()    # driver instance with User Interface (not headless)
+        self.browser=set_up_driver_instance() # driver instance without User Interface (--headless)
         self.pattern=CheckPattern(self.browser)
         self.game_play=PlayGame(self.browser,market=test_market)
         self.log=LoginUser(self.browser,username=os.environ.get("BETKING_USERNAME"),password=os.environ.get("BETKING_PASSWORD"))
@@ -27,29 +28,32 @@ class BrainTest(unittest.TestCase):
                         14200, 20250, 29000, 41500, 59250, 84750, 121250]
 
     
-    def test_choose_market(self):
-        time.sleep(2)
-        for _ in range(2):
-            self.game_play.choose_market()
-            time.sleep(10)
-        # time.sleep(10)
-        # delete_cache(self.browser)
-        # print('i have cleared the cache')
-        # time.sleep(100)
+    # def test_choose_market(self):
+    #     time.sleep(2)
+    #     for _ in range(2):
+    #         self.game_play.choose_market()
+    #         time.sleep(10)
+
+    #     # time.sleep(10)
+    #     # delete_cache(self.browser)
+    #     # print('i have cleared the cache')
+    #     # time.sleep(100)
 
 
     def test_select_stake_options_and_place_the_bet(self):
-        # check_result={'outcome':'4 - 1'}
-        check_result={'outcome':'2/1'}
-        stake_option_length=18
-        
+        check_result={'outcome':'4 - 1'}
+        # check_result={'outcome':'2/1'}
         time.sleep(2)
         self.game_play.choose_market()
-        self.test_login()
+        time.sleep(2)
+        acc_bal=self.log.login()
         acc_bal=2000.2
         # acc_bal=str(acc_bal)
-        # self.pattern.check_result(length="last/ result",latest_week="Week 2",acc_balance=acc_bal,market=check_result['outcome'])
-
+        week_to_play = self.browser.find_elements(By.CSS_SELECTOR, '.week')[0].text
+        print(week_to_play)
+        check_week=f"{week_to_play[:4]}{week_to_play[4:7]}"
+        print(check_week)
+        self.pattern.check_result(length="last result",latest_week=check_week,acc_balance=acc_bal,market=check_result['outcome'])
         for n in range(3):
             # clear_bet_slip(self.browser)
             if n==10:
@@ -60,59 +64,50 @@ class BrainTest(unittest.TestCase):
             
             week_selected=result[0]
             acc_bal=result[1]
-            # try:
-            #     acc_bal=self.game_play.place_the_bet(amount=self.AMOUNT_LIST[n],test=os.environ.get("TEST"))
-            # except:
-            #     pass
-            # print(acc_bal)
-            # if n==1:                 # To test the try & except block if results are not available
-            #     acc_bal="2,266"
             reduced_week_selected=reduce_week_selected(week_selected,by=0,league="bundliga")
-            # self.pattern.check_result(length="last result",latest_week=reduced_week_selected)
-            # self.pattern=CheckPattern(self.browser,market="ht/ft")
             self.pattern.check_result(length="last result",latest_week=reduced_week_selected,acc_balance=acc_bal,market=check_result['outcome'])
 
-    def test_checkout_virtual(self):
-        for _ in range(3):
-            self.browser.get("https://m.betking.com")
-            self.pattern.checkout_virtual(league="bundliga")
-            time.sleep(5)
+    # def test_checkout_virtual(self):
+    #     for _ in range(3):
+    #         self.browser.get("https://m.betking.com")
+    #         self.pattern.checkout_virtual(league="bundliga")
+    #         time.sleep(5)
 
 
-    def test_login(self):
-        acc_bal=self.log.login()
-        # acc_bal=float(acc_bal.replace(",","_"))
-        # GAME_LEVEL=round((acc_bal-1000)/500,2)
-        print(acc_bal)
-        # print(GAME_LEVEL)
-        time.sleep(3)
-        return acc_bal
+    # def test_login(self):
+    #     acc_bal=self.log.login()
+    #     # acc_bal=float(acc_bal.replace(",","_"))
+    #     # GAME_LEVEL=round((acc_bal-1000)/500,2)
+    #     print(acc_bal)
+    #     # print(GAME_LEVEL)
+    #     time.sleep(3)
+    #     return acc_bal
         
     
-    def test_check_last_result(self):
-        check_result={'outcome':'4 - 0'}
-        self.game_play.choose_market()
-        # acc_bal=self.test_login()
-        acc_bal="40000"
-        while True:
-            week_selected=self.game_play.select_stake_options(week="current_week",previous_week_selected="Week 1000",
-                                                              pattern_stake=self.pattern_stake_options[check_result['outcome']],stake_amount=self.AMOUNT_LIST[0])
-            reduced_week_selected=reduce_week_selected(week_selected,by=0,league="bundliga")
-            time.sleep(3)
-            clear_bet_slip(self.browser)
-            if self.pattern.check_result(length="last result",latest_week=reduced_week_selected,acc_balance=acc_bal,market=check_result['outcome'])['outcome']:
-                print(f"it came in {reduced_week_selected}")
-                break
-            # break
+    # def test_check_last_result(self):
+    #     check_result={'outcome':'4 - 0'}
+    #     self.game_play.choose_market()
+    #     # acc_bal=self.test_login()
+    #     acc_bal="40000"
+    #     while True:
+    #         week_selected=self.game_play.select_stake_options(week="current_week",previous_week_selected="Week 1000",
+    #                                                           pattern_stake=self.pattern_stake_options[check_result['outcome']],stake_amount=self.AMOUNT_LIST[0])
+    #         reduced_week_selected=reduce_week_selected(week_selected,by=0,league="bundliga")
+    #         time.sleep(3)
+    #         clear_bet_slip(self.browser)
+    #         if self.pattern.check_result(length="last result",latest_week=reduced_week_selected,acc_balance=acc_bal,market=check_result['outcome'])['outcome']:
+    #             print(f"it came in {reduced_week_selected}")
+    #             break
+    #         # break
 
 
 
     # TODO: check if theres is need to create another CheckPattern instance inorder for check_last_result to run sucessfully after the check_all_result has been run which will destroy and create a new driver instance
-    def test_check_all_result(self):
-        self.browser=self.pattern.check_result(length="all result", latest_week="all",to_play=14)['driver']
+    # def test_check_all_result(self):
+    #     self.browser=self.pattern.check_result(length="all result", latest_week="all",to_play=14)['driver']
 
-        # self.pattern=CheckPattern(self.browser,market="ht/ft")
-        # print(self.pattern.check_result(length="last result",latest_week="Week 21")['outcome']) 
+    #     # self.pattern=CheckPattern(self.browser,market="ht/ft")
+    #     # print(self.pattern.check_result(length="last result",latest_week="Week 21")['outcome']) 
 
 
 
@@ -121,3 +116,5 @@ class BrainTest(unittest.TestCase):
         
 if __name__=="__main__":
     unittest.main()
+    # test=BrainTest()
+    # test.test_select_stake_options_and_place_the_bet()
