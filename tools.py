c8a9a4eeab4e786e5efff5142c5da4763b1676c1
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-07-29 14:08:00 +0100

    activated headless option

--- tools.py
+++ tools.py
@@ -34,7 +34,7 @@ def set_up_driver_instance():
     chrome_options.add_experimental_option('useAutomationExtension', False)
 
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument('--log-level=3') # to stop printing error messages to the console 
     chrome_options.add_argument("start-maximized") # chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_argument("--disable-gpu")
